c10b8290b79ab0a82cf174b0f554cf0247991ec0
hami-r <mynameissaitama54@gmail.com>
2022-11-28 20:18:19 +0530

    values read from student entry page

--- src/app/student-entry/student-entry.component.ts
+++ src/app/student-entry/student-entry.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-entry.component.css']
 })
 export class StudentEntryComponent {
+  
+  name = ""
+  rollNo = ""
+  admissionNo = ""
+  mobileNo = ""
+  college = ""
+  parentName = ""
+  parentPhoneNo = ""
+  username = ""
+  password = ""
 
+  readValues = () => {
+    let data = {
+      "name":this.name,
+      "rollNo":this.rollNo,
+      "admissionNo":this.admissionNo,
+      "mobileNo":this.mobileNo,
+      "college":this.college,
+      "parentName":this.parentName,
+      "parentPhoto":this.parentPhoneNo,
+      "username":this.username,
+      "password":this.password
+    }
+
+    console.log(data);
+    
+  }
 }
